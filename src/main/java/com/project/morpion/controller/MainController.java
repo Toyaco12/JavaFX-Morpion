e0467bcf2fb40985ee36f0ce61dd8ed32214199c
Erwan Vinet <erwanvinet034@gmail.com>
2024-02-21 11:50:08 +0100

    modif settings modal

--- src/main/java/com/project/morpion/controller/MainController.java
+++ src/main/java/com/project/morpion/controller/MainController.java
@@ -42,120 +42,6 @@ public class MainController {
     }
 
 
-//    public void openSettings(ActionEvent actionEvent) {
-//        AtomicBoolean save = new AtomicBoolean(false);
-//        Stage modalStage = new Stage();
-//        modalStage.initModality(Modality.APPLICATION_MODAL);
-//        modalStage.setTitle("Settings Window");
-//
-//        GridPane gridPane = new GridPane();
-//        gridPane.setPadding(new Insets(10));
-//        gridPane.setHgap(5);
-//        gridPane.setVgap(5);
-//
-//        // Labels pour les lignes
-//        Label labelRow1 = new Label("FACILE");
-//        Label labelRow2 = new Label("MOYEN");
-//        Label labelRow3 = new Label("DIFFICILE");
-//
-//        // Ajout des labels pour les lignes
-//        gridPane.add(labelRow1, 0, 1);
-//        gridPane.add(labelRow2, 0, 2);
-//        gridPane.add(labelRow3, 0, 3);
-//
-//        // Labels pour les colonnes
-//        Label labelColumn1 = new Label("Hidden Layer Size");
-//        Label labelColumn2 = new Label("Number of Hidden Layers");
-//        Label labelColumn3 = new Label("Learning Rate");
-//
-//        // Ajout des labels pour les colonnes
-//        gridPane.add(labelColumn1, 1, 0);
-//        gridPane.add(labelColumn2, 2, 0);
-//        gridPane.add(labelColumn3, 3, 0);
-//
-//        String[][] settings = getSettings();
-//
-//        for (int i = 0; i < 3; i++) {
-//            for (int j = 0; j < 3; j++) {
-//                TextField textField = createTextField(settings[i][j+1]);
-//                gridPane.add(textField, j + 1, i + 1);
-//            }
-//        }
-//
-//        // Centrage du GridPane
-//        StackPane modalLayout = new StackPane();
-//        Button saveButton = new Button("Save Settings");
-//        saveButton.setOnAction(e -> {
-//            String[] set = {"F:", "M:", "D:"};
-//            for(Node n : gridPane.getChildren()){
-//                if(n instanceof TextField){
-//                    TextField tmp = (TextField) n;
-//                    int rowIndex = GridPane.getRowIndex(tmp);
-//                    int colIndex = GridPane.getColumnIndex(tmp);
-//                    if(colIndex == 3){
-//                        set[rowIndex - 1] += tmp.getText();
-//                    }
-//                    else{
-//                        set[rowIndex - 1] += tmp.getText() + ":";
-//                    }
-//                }
-//            }
-//            setSettings(set);
-//            save.set(true);
-//        });
-//
-//        Button closeButton = new Button("Close Settings");
-//        closeButton.setOnAction(e -> {
-//            String[] set = {"F:", "M:", "D:"};
-//            for(Node n : gridPane.getChildren()){
-//                if(n instanceof TextField){
-//                    TextField tmp = (TextField) n;
-//                    int rowIndex = GridPane.getRowIndex(tmp);
-//                    int colIndex = GridPane.getColumnIndex(tmp);
-//                    if(colIndex == 3){
-//                        set[rowIndex - 1] += tmp.getText();
-//                    }
-//                    else{
-//                        set[rowIndex - 1] += tmp.getText() + ":";
-//                    }
-//                }
-//            }
-//            String[] prev = {"F:" + settings[0][1] + ":" + settings[0][2] + ":" + settings[0][3], "M:" + settings[1][1] + ":" + settings[1][2] + ":" + settings[1][3], "D:" + settings[2][1] + ":" + settings[2][2] + ":" + settings[2][3]};
-//            if(Arrays.equals(set, prev)){
-//                modalStage.close();
-//            }
-//            else if (!save.get()){
-//                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-//                alert.setTitle("Quitter sans Sauvegarder");
-//                alert.setHeaderText("Confirmation nécessaire");
-//                alert.setContentText("Êtes-vous sûr de vouloir quiter sans sauvegarder ?");
-//
-//                alert.showAndWait().ifPresent(response -> {
-//                    if(response == ButtonType.OK){
-//                        modalStage.close();
-//                    }
-//                });
-//            }
-//            else{
-//                modalStage.close();
-//            }
-//        });
-//
-//        modalLayout.getChildren().addAll(gridPane);
-//        StackPane.setAlignment(gridPane, javafx.geometry.Pos.CENTER);
-//        VBox vBox = new VBox();
-//        vBox.getChildren().addAll(modalLayout, closeButton, saveButton); // Ajout du GridPane et du bouton "Close Modal"
-//        vBox.setPadding(new Insets(10));
-//        vBox.setSpacing(10);
-//        vBox.setAlignment(javafx.geometry.Pos.BOTTOM_CENTER);
-//
-//        Scene modalScene = new Scene(vBox, 600, 200);
-//        modalStage.setScene(modalScene);
-//        modalStage.setResizable(false);
-//        modalStage.showAndWait();
-//    }
-
-
     public String[][] getSettings() {
         try {
             FileReader settingFile = new FileReader("src/main/resources/com/project/morpion/ai/config.txt");
@@ -183,23 +69,6 @@ public class MainController {
         } catch (IOException ignored) { }
     }
 
-    private TextField createTextField(String setText){
-        TextField textField = new TextField(setText);
-
-        UnaryOperator<TextFormatter.Change> filter = change -> {
-            String text = change.getText();
-            if (Pattern.matches("[0-9]*", text)) {
-                return change;
-            }
-            return null;
-        };
-
-        TextFormatter<String> textFormatter = new TextFormatter<>(filter);
-        textField.setTextFormatter(textFormatter);
-
-        return textField;
-    }
-
     public void openLearning(ActionEvent actionEvent) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("view/learn.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
@@ -221,6 +90,7 @@ public class MainController {
         Scene scene = new Scene(fxmlLoader.load());
         Stage stageSettings = new Stage();
         stageSettings.setResizable(false);
+        stageSettings.setTitle("Model Settings");
         stageSettings.setScene(scene);
         stageSettings.show();
     }
