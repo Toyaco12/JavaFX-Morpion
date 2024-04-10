cc4d19e3efef36d3e2e908d4ccef5df3dc1a1d2e
Armaghan <theo.armaghan@alumni.univ-avignon.fr>
2024-04-10 18:20:35 +0200

    add visual of actual difficulty in model view

--- src/main/java/com/project/morpion/model/ItemModel.java
+++ src/main/java/com/project/morpion/model/ItemModel.java
@@ -1,5 +1,8 @@
 package com.project.morpion.model;
 
+import com.project.morpion.model.ai.Config;
+import com.project.morpion.model.ai.ConfigFileLoader;
+
 import java.io.File;
 
 public class ItemModel {
@@ -9,6 +12,24 @@ public class ItemModel {
     public ItemModel(String fullPath) {
         this.fullPath = fullPath;
         this.name = new File(fullPath).getName();
+        ConfigFileLoader cfl = new ConfigFileLoader();
+        cfl.loadConfigFile("src/main/resources/com/project/morpion/ai/config.txt");
+        Config configF = cfl.get("F");
+        Config configM = cfl.get("M");
+        Config configD = cfl.get("D");
+        String actualDifficultyF = new File("src/main/resources/com/project/morpion/ai/models/"+"F"+"/model_"+configF.hiddenLayerSize+"_"+configF.learningRate+"_"+configF.numberOfhiddenLayers+".srl").getName();
+        String actualDifficultyD = new File("src/main/resources/com/project/morpion/ai/models/"+"D"+"/model_"+configD.hiddenLayerSize+"_"+configD.learningRate+"_"+configD.numberOfhiddenLayers+".srl").getName();
+        String actualDifficultyM = new File("src/main/resources/com/project/morpion/ai/models/"+"M"+"/model_"+configM.hiddenLayerSize+"_"+configM.learningRate+"_"+configM.numberOfhiddenLayers+".srl").getName();
+        if (this.name.equals(actualDifficultyF)){
+            this.name +=" [F]";
+        }
+        if (this.name.equals(actualDifficultyM)){
+            this.name +=" [M]";
+        }
+        if (this.name.equals(actualDifficultyD)){
+            this.name +=" [D]";
+        }
+
     }
 
     public String getName() {
